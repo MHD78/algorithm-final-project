35c2c0af01cf09693b943aa4f19d349593fba811
mhd78m <mdamirchi27@gmail.com>
2022-07-05 23:29:55 +0430

    DFS algorithm

---
+++ DepthFirstSearch.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+
+class GraphDFS:
+    def __init__(self):
+        self.graph = defaultdict(list)
+        self.result = []
+
+    def __repr__(self):
+        rep = self.graph
+        return str(rep)
+
+    # converting inputed matrix data into dictionary
+    def edgeHandler(self,matrix):
+        for i in range(len(matrix)):
+            for j in range(len(matrix)):
+                if matrix[i][j] == 1:
+                    self.graph[i].append(j)
+        
+
+    # A function for chechking all nodes
+    def nodeCheck(self, v, visited):
+        visited.add(v)
+        self.result.append(v)
+        for neighbour in self.graph[v]:
+            if neighbour not in visited:
+                self.nodeCheck(neighbour, visited)
+
+    # recursive DFS traversal for all nodes
+    def DFS(self, v):
+        visited = set()
+        self.nodeCheck(v, visited)
+
+        if len(self.result) == len(self.graph):
+            print("DFS traversal order is : ",self.result)
+
+
+stratVertex = int(input("start vertex : "))
+matrixRows = int(input("matrix rows : "))
+print("input graph matrix :")
+matrix = []
+for i in range(matrixRows):    
+	single_row = list(map(int, input().split()))  
+	matrix.append(single_row)
+
+instance = GraphDFS()
+instance.edgeHandler(matrix)
+instance.DFS(stratVertex)
+
