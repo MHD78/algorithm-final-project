d032d68d53c01e34b0b4ed12b019998908fe0ae7
mhd78m <mdamirchi27@gmail.com>
2022-07-06 14:34:12 +0430

    adding documentation to / route

--- API/app.py
+++ API/app.py
@@ -1,3 +1,4 @@
+from cmath import inf
 from flask import Flask, jsonify, request
 from LCSApi import lcsAlgo
 from JobSchedulingApi import jobScheduling
@@ -12,21 +13,22 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return '''
-           <form method="POST " action="/Strassen">
-               <input type="submit" value="Strassen">
-           </form>
-           <form method="POST " action="/LCS">
-               <input type="submit" value="LCS">
-           </form>
-           <form method="POST " action="/JobScheduling">
-               <input type="submit" value="Job Scheduling">
-           </form>
+            <h2 style="color:blue;font-size:28px;" > endpoints : <h2/>
+            <ul>
+                <li><span style="color:DodgerBlue;font-size:24px;">/strassen    </span><span style="color:Tomato" >method=>POST</span><span style="color:Gray" >input value should be in JSON format //eg \"{"A":[[],...,[]] , "B":[[],...,[]]}\"</span></li>
+                <li><span style="color:DodgerBlue;font-size:24px;">/LCS?seq1=value&seq2=value   </span><span style="color:Tomato" >method=>GET</span></li>
+                <li><span style="color:DodgerBlue;font-size:24px;">/jobScheduling/count of jobs(user input)?jobs    </span><span style="color:Tomato" >method=>GET</span><span style="color:Gray" >//eg /2?job1=12&job2=22</span></li>
+                <li><span style="color:DodgerBlue;font-size:24px;">/backtrackKnapsack   </span><span style="color:Tomato" >method=>POST</span><span style="color:Gray" >input value should be in JSON format //eg \"{"capacity":20 , "weights":[10,5,20] ,"values":[5,7,3] }\"</span></li>
+                <li><span style="color:DodgerBlue;font-size:24px;">/branchAndBoundKnapsack    </span><span style="color:Tomato" >method=>POST</span><span style="color:Gray" >input value should be in JSON format //eg \"{"capacity":20 , "weights":[10,5,20] ,"values":[5,7,3] }\"</span></li>
+                <li><span style="color:DodgerBlue;font-size:24px;">/DFS   </span><span style="color:Tomato" >method=>POST</span><span style="color:Gray" >input value should be in JSON format //eg \"{"matrix":[] , "startVertex":2 }\"</span></li>
+                <li><span style="color:DodgerBlue;font-size:24px;">/bellmanFord    </span><span style="color:Tomato" >method=>POST</span><span style="color:Gray" >input value should be in JSON format //eg \"{"matrix":[] , "vertices":4 , "startVertex":2 }\"</span></li>
+            </ul>
            '''
-
-
-@app.route('/Strassen', methods=['GET'])
-def Strassen():
-    return "this is strassen matrix miltiplication code "
+@app.route("/strassen",methods=['POST'])
+def strassen():
+    a = request.json['A']
+    b = request.json['B']
+    return jsonify({"result":str(getMatrices(a,b))})
 
 @app.route('/LCS', methods=['GET'])
 def LCS():
@@ -35,33 +37,27 @@ def LCS():
 
     return jsonify(lcsAlgo(seq1,seq2))
 
-@app.route('/JobScheduling/<count>', methods=['GET'])
-def JobScheduling(count):
+@app.route('/jobScheduling/<count>', methods=['GET'])
+def jobScheduling(count):
     jobs =[ ("job"+str(x) , int(request.args[f"job{x}"])) for x in range(1,int(count)+1) ]
-     
+
     return jsonify(jobScheduling(jobs))
 
-@app.route('/BacktrackKnapsack/<count>', methods=['GET'])
-def BacktrackKnapsack(count):
-    capacity = int(request.args['c'])
-    weights =[ int(request.args[f"w{x}"]) for x in range(1,int(count)+1) ]
-    values =[  int(request.args[f"v{x}"]) for x in range(1,int(count)+1) ]
-     
-    return jsonify(resultHandler(int(count),capacity,weights,values))
+@app.route('/backtrackKnapsack', methods=['POST'])
+def backtrackKnapsack():
+    capacity = request.json['capacity']
+    weights = request.json['weights']
+    values = request.json['weights']
 
-@app.route('/BranchAndBoundKnapsack/<count>', methods=['GET'])
-def BranchAndBoundKnapsack(count):
-    capacity = int(request.args['c'])
-    weights =[ int(request.args[f"w{x}"]) for x in range(1,int(count)+1) ]
-    values =[  int(request.args[f"v{x}"]) for x in range(1,int(count)+1) ]
-     
-    return jsonify(bresultHandler(int(count),capacity,weights,values))
+    return jsonify(resultHandler(int(len(weights)),capacity,weights,values))
 
-@app.route("/strassen",methods=['POST'])
-def strassen():
-    a = request.json['A']
-    b = request.json['B']
-    return jsonify({"result":str(getMatrices(a,b))})
+@app.route('/branchAndBoundKnapsack', methods=['POST'])
+def branchAndBoundKnapsack():
+    capacity = request.json['capacity']
+    weights = request.json['weights']
+    values = request.json['weights']
+
+    return jsonify(bresultHandler(int(len(weights)),capacity,weights,values))
 
 @app.route("/DFS",methods=['POST'])
 def DFS():
@@ -75,6 +71,9 @@ def bellmanFord():
     g = Graph(request.json["vertices"])
     g.edgeHandler(request.json["matrix"])
     result = g.bellmanFord(request.json["startVertex"])
+    for key in result:
+        if result[key] == inf:
+            result[key] = "Infinity"
     return jsonify({"result":result})
 
 
